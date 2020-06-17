06623a476f018890cf1f06f8fa353af2ed659919
AntoMonti <a.matviishyn@hotmail.com>
2020-06-17 08:52:17 +0300

    adding elementary create action and params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,15 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def create
+    Item.create(items_params)
+    render body: 'created'
+  end
+
+  private
+
+  def items_params
+    params.permit(:name, :price, :real, :weight, :description)
+  end
 end
