188bdb45d154aba4dd94a6ddc50e4aae3644d8c0
AntoMonti <a.matviishyn@hotmail.com>
2020-06-19 20:59:47 +0300

    writing all actions for Item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,14 +4,56 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def create
-    Item.create(items_params)
-    render body: 'created'
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to @item
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(items_params)
+      redirect_to @item
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Item.find(params[:id]).destroy
+    redirect_to @item
+  end
+
+  def upvote
+    @item = Item.find(params[:id])
+    @item.increment! :votes_count
+    redirect_to items_path
+  end
+
+  def expensive
+    @items = Item.where('price > 50')
+    render :index
   end
 
   private
 
   def items_params
-    params.permit(:name, :price, :real, :weight, :description)
+    params.require(:item).permit(:name, :price, :real, :weight, :description)
   end
 end
