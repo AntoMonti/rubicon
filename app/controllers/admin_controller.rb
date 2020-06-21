a784523bfcebbe8d2f634a2c4d2e43827b98de3b
AntoMonti <a.matviishyn@hotmail.com>
2020-06-21 17:00:24 +0300

    creating admins toys

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,11 @@
 class AdminController < ApplicationController
+  before_action :admin?
+
+  def admin?
+    render file: 'public/500.html' unless params[:admin]
+  end
+      
+  def users_count
+    @users_count = User.count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
     get :upvote, on: :member
     get :expensive, on: :collection
   end
+  get '/admin/users_count', to: 'admin#users_count'
 end
