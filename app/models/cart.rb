ee572dfd2ed2126a554ac29cb79100d918ebda41
AntoMonti <a.matviishyn@hotmail.com>
2020-06-21 06:39:51 +0300

    creating relations between cart and item through position

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,5 @@
 class Cart < ApplicationRecord
   belongs_to :user, optional: true
   has_and_belongs_to_many :items
+  has_ many :positions
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
   validates :name, :description, presence: true
 
   has_and_belongs_to_many :carts
+  has_many :positions
 end
