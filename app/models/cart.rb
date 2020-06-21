d5d0d8a07107424a0021e06d47a1fcecc83e86df
AntoMonti <a.matviishyn@hotmail.com>
2020-06-21 06:47:19 +0300

    adding relations between cart and item again

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,6 @@
 class Cart < ApplicationRecord
   belongs_to :user, optional: true
-  has_and_belongs_to_many :items
+  
   has_ many :positions
+  has_many :items, through: :positions
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :name, :description, presence: true
 
-  has_and_belongs_to_many :carts
+  #has_and_belongs_to_many :carts
   has_many :positions
 end
