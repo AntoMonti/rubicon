29fd11d46822a6c003e84a15eef2834cc847c8ce
AntoMonti <a.matviishyn@hotmail.com>
2020-06-21 05:16:45 +0300

    creating relations between cart and item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :name, :description, presence: true
+
+  has_and_belongs_to_many :carts
 end
