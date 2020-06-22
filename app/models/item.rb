870667f26c107f5d9202744a4925c9370c98004e
AntoMonti <a.matviishyn@hotmail.com>
2020-06-22 20:15:03 +0300

    adding validates presence true for price and weight

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0 }
-  validates :name, :description, presence: true
+  validates :name, :price, :weight, :description, presence: true
 
   has_many :positions
   has_many :carts, through: :positions
