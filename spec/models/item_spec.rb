3b0477bce1fab8b8d36fa7dc50e202781fd0369c
AntoMonti <a.matviishyn@hotmail.com>
2020-06-23 13:48:47 +0300

    creating rspec tests for item model

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  it 'validate strong params' do
+    should validate_presence_of(:name)
+    should validate_presence_of(:price)
+    should validate_presence_of(:weight)
+    should validate_presence_of(:description)
+  end
+
+  it 'validate models attributes' do
+    should validate_numericality_of(:price).
+      is_greater_than_or_equal_to(0)
+  end
+
+  it 'validate relations' do
+    should have_many(:positions)
+    should have_many(:carts).through(:positions)
+    should have_many(:comments)
+    should have_one(:image)
+  end 
+end
